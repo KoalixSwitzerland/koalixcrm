c349fc18f9c0b28b9417279b28efbee6b47eaa70
Aaron Riedener <aaron@aaron-desktop>
2010-11-19 18:57:39 +0000

    added const values for djangoUeserExtention

---
+++ djangoUserExtention/const/purpose.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*
+
+from django.utils.translation import ugettext as _
+
+PURPOSESADDRESSINUSEREXTENTION = (
+    ('H', _('Private')),
+    ('O', _('Business')),
+    ('P', _('Mobile Private')),
+    ('B', _('Mobile Business')),
+)
