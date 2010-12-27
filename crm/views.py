6606aedd84e807a44b647e21854fc9a326e0463a
Aaron Riedener <aaron@aaron-desktop>
2010-12-27 07:57:44 +0000

    Adjusted registring of invoices

--- crm/views.py
+++ crm/views.py
@@ -4,16 +4,24 @@ from crm.models import *
 from django.http import HttpResponse
 from django.core.servers.basehttp import FileWrapper
 
-def createQuotePDF(request, quoteid):
+def createQuotePDF(request, seladdress, quoteid, salesorderaddressid):
    quote = Quote.objects.get(id=quoteid)
    pdf = quote.createPDF(purchaseconfirmation=False)
    response = HttpResponse(FileWrapper(file(pdf)), mimetype='application/pdf')
    response['Content-Length'] = path.getsize(pdf)
    return response
    
-def createInvoicePDF(request, invoiceid):
-   invoice = Invoice.objects.get(id=invoiceid)
-   pdf = invoice.createPDF(deliveryorder=False)
-   response = HttpResponse(FileWrapper(file(pdf)), mimetype='application/pdf')
-   response['Content-Length'] = path.getsize(pdf)
-   return response
+def createInvoicePDF(request, seladdress, invoiceid, salesorderaddressid):
+  if seladdress == 'true' :
+    selectaddress(invoiceid)
+  else:
+    invoice = Invoice.objects.get(id=invoiceid)
+    pdf = invoice.createPDF(deliveryorder=False)
+    response = HttpResponse(FileWrapper(file(pdf)), mimetype='application/pdf')
+    response['Content-Length'] = path.getsize(pdf)
+    return response
+   
+def selectaddress(invoiceid):
+  invoice = Invoice.objects.get(id=invoiceid)
+  address = invoice.contract
+   
