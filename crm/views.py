cc06de1fce729f6b26a0c2cdb8efd5a14c24c158
Aaron Riedener <aaron@ariedene-desktop.(none)>
2010-12-31 00:40:48 +0100

    In order to be able to release an alpha version I fixed a bug in the pdf export functions

--- crm/views.py
+++ crm/views.py
@@ -4,14 +4,14 @@ from crm.models import *
 from django.http import HttpResponse
 from django.core.servers.basehttp import FileWrapper
 
-def createQuotePDF(request, seladdress, quoteid, salesorderaddressid):
+def createQuotePDF(request, quoteid):
    quote = Quote.objects.get(id=quoteid)
    pdf = quote.createPDF(purchaseconfirmation=False)
    response = HttpResponse(FileWrapper(file(pdf)), mimetype='application/pdf')
    response['Content-Length'] = path.getsize(pdf)
    return response
    
-def createInvoicePDF(request, seladdress, invoiceid, salesorderaddressid):
+def createInvoicePDF(request, invoiceid):
   if seladdress == 'true' :
     selectaddress(invoiceid)
   else:
