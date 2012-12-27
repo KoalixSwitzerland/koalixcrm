00ee512ed3adaa6d170edfa3a46adefd199e7878
Aaron Riedener <aaron.riedener@gmail.com>
2012-12-27 13:36:35 +0100

    Fixed balancesheet and profit-loss-statement export error

--- accounting/views.py
+++ accounting/views.py
@@ -5,15 +5,21 @@ from django.http import HttpResponse
 from django.core.servers.basehttp import FileWrapper
 
 def createBalanceSheetPDF(request, calculationunitid):
-   calculationUnit = AccountingCalculationUnit.objects.get(id=calculationunitid)
-   pdf = calculationUnit.createBalanceSheetPDF(request.user)
+  try:
+   accountingPeriod = AccountingPeriod.objects.get(id=quoteid)
+   pdf = accountingPeriod.createBalanceSheetPDF(request.user)
    response = HttpResponse(FileWrapper(file(pdf)), mimetype='application/pdf')
    response['Content-Length'] = path.getsize(pdf)
    return response
-   
+  except (TemplateSetMissing, UserExtensionMissing), e:
+    raise Http404
+
 def createProfitLossStatementPDF(request, calculationunitid):
-   calculationUnit = AccountingCalculationUnit.objects.get(id=calculationunitid)
-   pdf = calculationUnit.createProfitLossStatementPDF(request.user)
+  try:
+   accountingPeriod = AccountingPeriod.objects.get(id=quoteid)
+   pdf = accountingPeriod.createProfitLossStatementPDF(request.user)
    response = HttpResponse(FileWrapper(file(pdf)), mimetype='application/pdf')
    response['Content-Length'] = path.getsize(pdf)
    return response
+  except (TemplateSetMissing, UserExtensionMissing), e:
+    raise Http404
