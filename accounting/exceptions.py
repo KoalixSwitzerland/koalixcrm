f4bed2885d4dee6bf39572af00d239e3b222048a
Aaron Riedener <aaron.riedener@gmail.com>
2014-10-19 21:51:37 +0200

    added missing file

---
+++ accounting/exceptions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class ProgrammingError(Exception):
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
+    
+class NoObjectsToBeSerialzed(Exception):
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
+        
