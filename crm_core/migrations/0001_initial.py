665b3b8df2f5e6328a402bbb8e606a87cb72e84c
tfroehlich82 <t.loop@gmx.net>
2015-08-05 23:39:26 +0200

    Try to fix build

--- crm_core/migrations/0001_initial.py
+++ crm_core/migrations/0001_initial.py
@@ -13,6 +13,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('shop', '0006_auto_20150804_2004'),
     ]
 
     operations = [
