4afffb37d894d33a1c6e1a650f053146f28bca79
tfroehlich82 <t.loop@gmx.net>
2015-08-26 22:56:08 +0200

    fixed datetimepicker

---
+++ static/assets/js/locales/bootstrap-datetimepicker.eu.js
@@ -0,0 +1,18 @@
+/**
+ * Basque translation for bootstrap-datetimepicker
+ * U. Odriozola <urtzi.odriozola@gmail.com>
+ */
+;(function($){
+    $.fn.datetimepicker.dates['eu'] = {
+        days: ["Igandea", "Astelehena", "Asteartea", "Asteazkena", "Osteguna", "Ostirala", "Larunbata", "Igandea"],
+        daysShort: ["Ig.","Al.",  "Ar.", "Az.", "Og.", "Ol.", "Lr.", "Ig."],
+        daysMin: ["ig", "al", "ar", "az", "og", "ol", "lr", "ig"],
+        months: ["Urtarrila", "Otsaila", "Martxoa", "Apirila", "Maiatza", "Ekaina", "Uztaila", "Abuztua", "Iraila", "Urria", "Azaroa", "Abendua"],
+        monthsShort: ["Urt", "Ots", "Mar", "Api", "Mai", "Eka", "Uzt", "Abu", "Ira", "Urr", "Aza", "Abe"],
+        today: "Gaur",
+        suffix: [],
+        meridiem: [],
+        format: "yyyy/mm/dd",
+        weekStart: 1
+    };
+}(jQuery));
