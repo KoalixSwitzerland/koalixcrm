26af701de812ebc6ce8cb0d1bb6935d8e087182b
Simon Riedener <simon.riedener@koalix.ch>
2017-08-10 22:49:53 +0200

    [issue #20] Partly revert misconfigured paths.

--- sandbox/test_koalixcrm/settings.py
+++ sandbox/test_koalixcrm/settings.py
@@ -131,12 +131,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 PDF_OUTPUT_ROOT = "/var/www/test_koalixcrm/static/pdf/"
 FILEBROWSER_DIRECTORY = "/var/www/test_koalixcrm/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
