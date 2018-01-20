d58f66fb4315ce06f37a7d5438d2465cb798bdbd
Aaron Riedener <aaron.riedener@gmail.com>
2018-01-20 20:45:42 +0100

    fixed issue in the setup.py spelling error

--- setup.py
+++ setup.py
@@ -10,10 +10,9 @@ setup(name='koalix-crm',
       packages=find_packages(exclude=["projectsettings", "documentation"]),
       install_requires=['Django==1.11.4','django-filebrowser-no-grappelli==3.7.2',
                         'lxml>=3.8.0','olefile>=0.44','Pillow>=4.2.1',
-                        'psycopg2>=2.7.3', 'pytz>=2017.2', 'dajngo-grappelli==2.10.1',],
+                        'psycopg2>=2.7.3', 'pytz>=2017.2', 'django-grappelli==2.10.1',],
       zip_safe=False,
       classifiers=['Development Status :: 4 - Beta','Programming Language :: Python :: 3.4',],
       python_requires='~=3.5',
       include_package_data=True,
 )
-
