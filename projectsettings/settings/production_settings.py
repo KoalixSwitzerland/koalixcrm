4c6d4e452459f2a682e3123cd9be60c3df3cf808
Simon Riedener <simon.riedener@koalix.ch>
2018-02-04 12:01:15 +0100

    [issue #97] Extended base settings by production_settings. - Database configuration - Allowed hosts configuration

---
+++ projectsettings/settings/production_settings.py
@@ -0,0 +1,17 @@
+from .base_settings import *
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+GRAPPELLI_INDEX_DASHBOARD = 'projectsettings.dashboard.CustomIndexDashboard'
