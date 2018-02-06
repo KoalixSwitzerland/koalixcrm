2225fc9365dfe84a4e94b12673b6116fe5456aa8
Aaron Riedener <aaron.riedener@gmail.com>
2018-02-06 21:10:39 +0100

    continued work on issue #83

--- koalixcrm/crm/reporting/employeeassignmenttotask.py
+++ koalixcrm/crm/reporting/employeeassignmenttotask.py
@@ -8,3 +8,12 @@ class EmployeeAssignmentToTask(models.Model):
     employee = models.ForeignKey('auth.User', limit_choices_to={'is_staff': True}, blank=True, verbose_name=_("Staff"),
                               related_name="db_relscstaff", null=True)
     planned_effort = models.TimeField(verbose_name=_("Effort"))
+    task = models.ForeignKey("Task", verbose_name=_('Task'), blank=False, null=False)
+
+    def __str__(self):
+        return _("Employee Assignment") + ": " + str(self.employee.first_name)
+
+    class Meta:
+        app_label = "crm"
+        verbose_name = _('Employee Assignment')
+        verbose_name_plural = _('Employee Assignments')
