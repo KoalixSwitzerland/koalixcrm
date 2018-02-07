05af31f77e810e64cb017a4f816c1b128327f274
Simon Riedener <simon.riedener@koalix.ch>
2018-02-07 23:51:47 +0100

    [issue #97] Added commands for FOP installation to entrypoint.sh - Changed line endings from CRLF to LF

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Install dependencies
 pip install -r development_requirements.txt
 
+# Install FOP 2.2
+wget https://archive.apache.org/dist/xmlgraphics/fop/source/fop-2.2-src.tar.gz
+tar -xzf fop-2.2-src.tar.gz -C ../usr/bin
+rm -rf fop-2.2-src.tar.gz
+
+# Execute startup scripts
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
