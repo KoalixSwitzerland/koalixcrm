baa6919fb22b854cdbe14ddd52b11bff7d58c9b3
Aaron Riedener <aaron.riedener@gmail.com>
2018-03-08 21:53:59 +0100

    - implemented registration of new work - implemented update of existing work

--- koalixcrm/crm/views/monthlyreport.py
+++ koalixcrm/crm/views/monthlyreport.py
@@ -24,7 +24,8 @@ class MonthlyReportView:
         date = forms.DateField(widget=AdminDateWidget)
         start_time = forms.TimeField(widget=AdminTimeWidget)
         stop_time = forms.TimeField(widget=AdminTimeWidget)
-        short_description = forms.CharField()
+        short_description = forms.CharField(required=False)
+        work_id = forms.IntegerField(widget=forms.HiddenInput(), required=False)
 
     @staticmethod
     def generate_initial_data(start_date, stop_date):
@@ -32,7 +33,8 @@ class MonthlyReportView:
         list_of_work = Work.objects.filter(date__lte=stop_date).filter(date__gte=start_date)
         initial = []
         for work in list_of_work:
-            initial.append({'task': work.task,
+            initial.append({'work_id': work.id,
+                            'task': work.task,
                             'projects': work.task.project,
                             'date': work.date,
                             'start_time': work.start_time,
@@ -42,28 +44,36 @@ class MonthlyReportView:
 
     def work_report(request):
         from koalixcrm.crm.reporting.work import Work
-        MonthlyReportFormset = forms.formset_factory(MonthlyReportView.MonthlyReportingForm, extra=5, max_num=60, can_delete=True)
+        monthly_report_formset = forms.formset_factory(MonthlyReportView.MonthlyReportingForm,
+                                                     extra=1, max_num=60, can_delete=True)
         if request.POST.get('post'):
-            formset = MonthlyReportFormset(request.POST)
+            formset = monthly_report_formset(request.POST)
             if 'cancel' in request.POST:
                 HttpResponseRedirect('/admin/')
             elif 'register' in request.POST:
                 if formset.is_valid():
                     for form in formset:
-                        new_work = Work()
-                        new_work.task = form.cleaned_data['task']
-                        new_work.employee = UserExtension.get_user(request.user)
-                        new_work.date = form.cleaned_data['date']
-                        new_work.start_time = datetime.datetime.combine(form.cleaned_data['date'],
-                                                               form.cleaned_data['start_time'])
-                        new_work.stop_time = datetime.datetime.combine(form.cleaned_data['date'],
-                                                              form.cleaned_data['stop_time'])
-                        new_work.short_description = form.cleaned_data['short_description']
-                        new_work.save()
+                        if form.cleaned_data['work_id']:
+                            work = Work.objects.get(id=form.cleaned_data['work_id'])
+                        else:
+                            work = Work()
+                        work.task = form.cleaned_data['task']
+                        work.employee = UserExtension.get_user(request.user)
+                        work.date = form.cleaned_data['date']
+                        work.start_time = datetime.datetime.combine(form.cleaned_data['date'],
+                                                           form.cleaned_data['start_time'])
+                        work.stop_time = datetime.datetime.combine(form.cleaned_data['date'],
+                                                          form.cleaned_data['stop_time'])
+                        work.short_description = form.cleaned_data['short_description']
+                        work.save()
+                else:
+                    raise InvaldidForm;
             return HttpResponseRedirect('/admin/')
         else:
-            formset = MonthlyReportFormset(initial=MonthlyReportView.generate_initial_data(datetime.datetime.today()-datetime.timedelta(days=30),
-                                                                                   datetime.datetime.today()))
+            date_now = datetime.datetime.today()
+            date_30days_ago = date_now-datetime.timedelta(days=30)
+            initial_data = MonthlyReportView.generate_initial_data(date_30days_ago, date_now)
+            formset = monthly_report_formset(initial=initial_data)
             c = {'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME, 'formset': formset}
             c.update(csrf(request))
             return render(request, 'crm/admin/time_reporting.html', c)
