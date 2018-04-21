cd89ddfe53acf40891dadf8cd40116e9fb1a4aaf
Aaron Riedener <aaron.riedener@gmail.com>
2018-04-21 22:41:21 +0200

    [Issue #124] Work arround solution ... longterm solution should follow soon

--- koalixcrm/crm/reporting/urls.py
+++ koalixcrm/crm/reporting/urls.py
@@ -3,8 +3,8 @@
 # DJANGO IMPORTS
 from django.conf.urls import url
 
-from koalixcrm.crm.views.monthlyreport import MonthlyReportView
+#from koalixcrm.crm.views.monthlyreport import MonthlyReportView
 
 urlpatterns = [
-    url(r'^monthlyreport/$', MonthlyReportView.work_report, name="monthly_report"),
+    #url(r'^monthlyreport/$', MonthlyReportView.work_report, name="monthly_report"),
 ]
