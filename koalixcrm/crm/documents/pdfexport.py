610de27d19b605798df2c749a54413ac27e3dc2a
Aaron Riedener <aaron.riedener@gmail.com>
2018-04-26 23:03:31 +0200

    [Issue #19] -to improve testability - prepared to serialize each model separately into an xml. Next step is to roll out the serialization into each model and then to write a test for each model separately

--- koalixcrm/crm/documents/pdfexport.py
+++ koalixcrm/crm/documents/pdfexport.py
@@ -18,12 +18,22 @@ import koalixcrm.djangoUserExtension.models
 class PDFExport:
 
     @staticmethod
-    def extend_xml_with_root_element(file_with_serialized_xml):
-        xml = etree.parse(file_with_serialized_xml)
-        root_element = xml.getroot()
+    def extend_xml_with_root_element(xml_string):
+        parser = etree.XMLParser(encoding='utf-8')
+        root_element = etree.fromstring(xml_string.encode('utf-8'), parser=parser)
         filebrowser_directory = etree.SubElement(root_element, "filebrowser_directory")
         filebrowser_directory.text = settings.MEDIA_ROOT
-        xml.write(file_with_serialized_xml)
+        return (etree.tostring(root_element, encoding='UTF-8', xml_declaration=True)).decode('utf-8')
+
+    @staticmethod
+    def merge_xml(xml_string_1, xml_string_2):
+        parser = etree.XMLParser(encoding='utf-8')
+        root_element_1 = etree.fromstring(xml_string_1.encode('utf-8'), parser=parser)
+        root_element_2 = etree.fromstring(xml_string_2.encode('utf-8'), parser=parser)
+        for child in root_element_2:
+            root_element_1.append(child)
+        return (etree.tostring(root_element_1, encoding='UTF-8', xml_declaration=True)).decode('utf-8')
+
 
     @staticmethod
     def create_list_of_objects_to_serialize(object_to_create_pdf):
@@ -35,12 +45,18 @@ class PDFExport:
             raise NoSerializationPatternFound(_("During "+str(object_to_create_pdf)+" PDF Export"))
 
     @staticmethod
-    def write_xml_file(objects_to_serialize, file_with_serialized_xml):
-        XMLSerializer = serializers.get_serializer("xml")
-        xml_serializer = XMLSerializer()
-        out = open(file_with_serialized_xml, "wb")
-        xml_serializer.serialize(objects_to_serialize, stream=out, indent=3)
-        out.close()
+    def write_xml(objects_to_serialize):
+        xml = serializers.serialize("xml", objects_to_serialize, indent=3)
+        return xml
+
+    @staticmethod
+    def write_xml_file(xml, file_path):
+        xml.encode('utf-8')
+        f = open(file_path, "w+")
+        f.truncate()
+        f.write(xml)
+        f.close()
+
 
     @staticmethod
     def perform_xsl_transformation(file_with_serialized_xml, xsl_file, fop_config_file, file_output_pdf):
@@ -60,18 +76,25 @@ class PDFExport:
         file_output_pdf = os.path.join(settings.PDF_OUTPUT_ROOT, (str(type(object_to_create_pdf).__name__) +
                                                                   "_" + str(object_to_create_pdf.id) + ".pdf"))
 
-        # list the sub-objects which to be serialized
+        # list the sub-objects which have to be serialized
         objects_to_serialize = PDFExport.create_list_of_objects_to_serialize(object_to_create_pdf)
-        objects_to_serialize += list(koalixcrm.djangoUserExtension.models.DocumentTemplate.objects.filter(id=template_set.id))
-        objects_to_serialize += koalixcrm.djangoUserExtension.models.UserExtension.objects_to_serialize(object_to_create_pdf, printed_by)
+        xml_string_1 = PDFExport.write_xml(objects_to_serialize)
+        objects_to_serialize = list(koalixcrm.djangoUserExtension.models.DocumentTemplate.objects.filter(id=template_set.id))
+        xml_string_2 = PDFExport.write_xml(objects_to_serialize)
+        objects_to_serialize = koalixcrm.djangoUserExtension.models.UserExtension.objects_to_serialize(object_to_create_pdf, printed_by)
+        xml_string_3 = PDFExport.write_xml(objects_to_serialize)
 
-        # serialize the objects to xml-file
-        PDFExport.write_xml_file(objects_to_serialize, file_with_serialized_xml)
+        xml_string = PDFExport.merge_xml(xml_string_1, xml_string_2)
+        xml_string = PDFExport.merge_xml(xml_string, xml_string_3)
 
-        # extend the xml-file with required basic settings
-        PDFExport.extend_xml_with_root_element(file_with_serialized_xml)
+        # extend the xml-string with required basic settings
+        xml_string = PDFExport.extend_xml_with_root_element(xml_string)
+
+        #  write xml-string to xml-file
+        PDFExport.write_xml_file(xml_string, file_with_serialized_xml)
 
         # perform xsl transformation
         PDFExport.perform_xsl_transformation(file_with_serialized_xml, xsl_file, fop_config_file, file_output_pdf)
+
 
         return file_output_pdf
