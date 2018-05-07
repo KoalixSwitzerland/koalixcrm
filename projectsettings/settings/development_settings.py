dd8164d297a0fa5a7743a589c4c1f0ed896ba71d
Simon Riedener <simon.riedener@koalix.ch>
2018-05-07 21:59:02 +0200

    [no-issue] Changed development database type to posgresql - avoid reset of database after updating the koalixcrm application

--- projectsettings/settings/development_settings.py
+++ projectsettings/settings/development_settings.py
@@ -6,8 +6,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
