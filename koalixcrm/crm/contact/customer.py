48dfa7e3c9b1de94eece74ae5a8f28bfc5135232
Aaron Riedener <aaron.riedener@gmail.com>
2018-05-07 21:58:09 +0200

    [Issue #127] -fixed some miner naming issues in the files

--- koalixcrm/crm/contact/customer.py
+++ koalixcrm/crm/contact/customer.py
@@ -12,11 +12,12 @@ from koalixcrm.crm.contact.contact import ContactEmailAddress
 
 import koalixcrm.crm.documents.contract
 
-class Customer(Contact):
-    defaultCustomerBillingCycle = models.ForeignKey('CustomerBillingCycle', verbose_name=_('Default Billing Cycle'))
-    ismemberof = models.ManyToManyField("CustomerGroup", verbose_name=_('Is member of'), blank=True)
 
-    def createContract(self, request):
+class Customer(Contact):
+    default_customer_billing_cycle = models.ForeignKey('CustomerBillingCycle', verbose_name=_('Default Billing Cycle'))
+    is_member_of = models.ManyToManyField("CustomerGroup", verbose_name=_('Is member of'), blank=True)
+
+    def create_contract(self, request):
         contract = koalixcrm.crm.documents.contract.Contract()
         contract.default_customer = self
         contract.default_currency = djangoUserExtension.models.UserExtension.objects.filter(user=request.user.id)[
@@ -26,19 +27,19 @@ class Customer(Contact):
         contract.save()
         return contract
 
-    def createInvoice(self):
-        contract = self.createContract()
-        invoice = contract.createInvoice()
+    def create_invoice(self):
+        contract = self.create_contract()
+        invoice = contract.create_invoice()
         return invoice
 
-    def createQuote(self):
-        contract = self.createContract()
-        quote = contract.createQuote()
+    def create_quote(self):
+        contract = self.create_contract()
+        quote = contract.create_quote()
         return quote
 
-    def isInGroup(self, customerGroup):
-        for customerGroupMembership in self.ismemberof.all():
-            if (customerGroupMembership.id == customerGroup.id):
+    def is_in_group(self, customer_group):
+        for customer_group_membership in self.is_member_of.all():
+            if customer_group_membership.id == customer_group.id:
                 return 1
         return 0
 
@@ -52,8 +53,8 @@ class Customer(Contact):
 
 
 class OptionCustomer(admin.ModelAdmin):
-    list_display = ('id', 'name', 'defaultCustomerBillingCycle',)
-    fieldsets = (('', {'fields': ('name', 'defaultCustomerBillingCycle', 'ismemberof',)}),)
+    list_display = ('id', 'name', 'default_customer_billing_cycle',)
+    fieldsets = (('', {'fields': ('name', 'default_customer_billing_cycle', 'is_member_of',)}),)
     allow_add = True
     ordering = ('id',)
     search_fields = ('id', 'name')
@@ -61,40 +62,40 @@ class OptionCustomer(admin.ModelAdmin):
     pluginProcessor = PluginProcessor()
     inlines.extend(pluginProcessor.getPluginAdditions("customerInline"))
 
-    def createContract(self, request, queryset):
+    def create_contract(self, request, queryset):
         for obj in queryset:
-            contract = obj.createContract(request)
+            contract = obj.create_contract(request)
             response = HttpResponseRedirect('/admin/crm/contract/' + str(contract.id))
             return response
 
-    createContract.short_description = _("Create Contract")
+    create_contract.short_description = _("Create Contract")
 
     @staticmethod
-    def createQuote(self, request, queryset):
+    def create_quote(self, request, queryset):
         for obj in queryset:
-            quote = obj.createQuote()
+            quote = obj.create_quote()
             response = HttpResponseRedirect('/admin/crm/quote/' + str(quote.id))
         return response
 
-    createQuote.short_description = _("Create Quote")
+    create_quote.short_description = _("Create Quote")
 
     @staticmethod
-    def createInvoice(self, request, queryset):
+    def create_invoice(self, request, queryset):
         for obj in queryset:
-            invoice = obj.createInvoice()
+            invoice = obj.create_invoice()
             response = HttpResponseRedirect('/admin/crm/invoice/' + str(invoice.id))
         return response
 
-    createInvoice.short_description = _("Create Invoice")
+    create_invoice.short_description = _("Create Invoice")
 
     def save_model(self, request, obj, form, change):
-        if (change == True):
-            obj.lastmodifiedby = request.user
+        if change:
+            obj.last_modified_by = request.user
         else:
-            obj.lastmodifiedby = request.user
+            obj.last_modified_by = request.user
             obj.staff = request.user
         obj.save()
 
-    actions = ['createContract', 'createInvoice', 'createQuote']
+    actions = ['create_contract', 'create_invoice', 'create_quote']
     pluginProcessor = PluginProcessor()
     inlines.extend(pluginProcessor.getPluginAdditions("customerActions"))
