02d1c1b8021b52afcd68612da948d8ff58630a49
Simon Riedener <simon.riedener@koalix.ch>
2018-06-25 23:54:34 +0200

    [issue #51] Removed account_activa and account_passiva until the separation of Accounting and CRM in this area is properly done.

--- koalixcrm/crm/product/tax.py
+++ koalixcrm/crm/product/tax.py
@@ -35,14 +35,9 @@ class OptionTax(admin.ModelAdmin):
 class TaxJSONSerializer(serializers.HyperlinkedModelSerializer):
     rate = serializers.CharField(source='tax_rate')
     description = serializers.CharField(source='name')
-    assetAccount = serializers.CharField(source='account_activa')
-    liabilityAccount = serializers.CharField(source='account_passiva')
-
 
     class Meta:
         model = Tax
         fields = ('id',
                   'rate',
-                  'description',
-                  'assetAccount',
-                  'liabilityAccount')
+                  'description')
