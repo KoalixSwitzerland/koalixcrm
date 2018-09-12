b3e6ae6783e8ce90806b1f1d878683bd405dc018
Aaron Riedener <aaron.riedener@gmail.com>
2018-09-12 19:32:59 +0200

    [Issue #n/a] the testcase with selenium did not lead to failed testcase but only to

--- koalixcrm/test_support_functions.py
+++ koalixcrm/test_support_functions.py
@@ -8,15 +8,14 @@ def assert_when_element_does_not_exist(testcase, xpath):
     try:
         testcase.selenium.find_element_by_xpath(xpath)
     except NoSuchElementException:
-        print(xpath+" does not exist")
+        testcase.assertTrue(True, xpath+" does not exist")
 
 
 def assert_when_element_exists(testcase, xpath):
     try:
         testcase.selenium.find_element_by_xpath(xpath)
     except NoSuchElementException:
-        return
-    print(xpath+" does exist")
+        testcase.assertTrue(True, xpath+" does exist")
 
 
 def make_date_utc(input_date):
