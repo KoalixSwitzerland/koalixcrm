1c533de1d94a9afb32f4107d65d827a98bd427ff
Aaron Riedener <aaron.riedener@gmail.com>
2018-10-19 22:04:44 +0200

    [Issue #215] fixed issue by adding a getter method which returns a safe value in case the values is not properly filled

--- koalixcrm/crm/product/currency.py
+++ koalixcrm/crm/product/currency.py
@@ -6,9 +6,28 @@ from django.utils.translation import ugettext as _
 
 
 class Currency(models.Model):
-    description = models.CharField(verbose_name=_("Description"), max_length=100)
-    short_name = models.CharField(verbose_name=_("Displayed Name After Price In The Position"), max_length=3)
-    rounding = models.DecimalField(max_digits=5, decimal_places=2, verbose_name=_("Rounding"), blank=True, null=True)
+    description = models.CharField(verbose_name=_("Description"),
+                                   max_length=100)
+    short_name = models.CharField(verbose_name=_("Displayed Name After Prices"),
+                                  max_length=3)
+    rounding = models.DecimalField(max_digits=5,
+                                   decimal_places=2,
+                                   verbose_name=_("Rounding"),
+                                   blank=True,
+                                   null=True)
+
+    def get_rounding(self):
+        """Returns either the stored rounding value for a currency or a default rounding value of 0.05
+
+        Args: no arguments
+
+        Returns: Decimal value
+
+        Raises: should not return exceptions"""
+        if self.rounding is None:
+            return 0.05
+        else:
+            return self.rounding
 
     def __str__(self):
         return self.short_name
@@ -20,6 +39,11 @@ class Currency(models.Model):
 
 
 class OptionCurrency(admin.ModelAdmin):
-    list_display = ('id', 'description', 'short_name', 'rounding')
-    fieldsets = (('', {'fields': ('description', 'short_name', 'rounding')}),)
+    list_display = ('id',
+                    'description',
+                    'short_name',
+                    'rounding')
+    fieldsets = (('', {'fields': ('description',
+                                  'short_name',
+                                  'rounding')}),)
     allow_add = True
