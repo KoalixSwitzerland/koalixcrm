c781c96801692c7f7871e9b669784324881a6582
Aaron Riedener <aaron.riedener@gmail.com>
2018-11-14 22:13:11 +0100

    [Issue #220] fixed Review finding, wrong default language

--- projectsettings/settings/base_settings.py
+++ projectsettings/settings/base_settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'pt_BR'
+LANGUAGE_CODE = 'en_US'
 
 TIME_ZONE = 'UTC'
 
