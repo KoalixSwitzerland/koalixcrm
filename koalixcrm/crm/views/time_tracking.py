b8b72e53da0f649fb88193be7fcc5360f81d176c
Aaron Riedener <aaron.riedener@gmail.com>
2019-01-06 17:35:10 +0100

    [Issue #239] again improved the bad lookup in the database

--- koalixcrm/crm/views/time_tracking.py
+++ koalixcrm/crm/views/time_tracking.py
@@ -19,10 +19,12 @@ from koalixcrm.crm.reporting.human_resource import HumanResource
 @login_required
 def work_report(request):
     try:
-        human_resource = HumanResource.objects.get(user=UserExtension.get_user_extension(request.user))
-        if not human_resource:
+        human_resource = HumanResource.objects.filter(user=UserExtension.get_user_extension(request.user))
+        if len(human_resource) == 0:
             error_message = "User "+request.user.__str__()+" is not registered as human resource"
             raise UserIsNoHumanResource(error_message)
+        else:
+            human_resource = HumanResource.objects.get(user=UserExtension.get_user_extension(request.user))
         if request.POST.get('post'):
             if 'cancel' in request.POST:
                 return HttpResponseRedirect('/admin/')
