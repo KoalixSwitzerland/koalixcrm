37db684c7f3a849d8890c98af6e8ae6d1cf949c7
Aaron Riedener <aaron.riedener@gmail.com>
2019-01-09 23:53:08 +0100

    [Issue #239] fixed all the testcases which failed after the update to Django 2.1 Implemented a work around for the issues

--- koalixcrm/crm/tests/test_task_update_last_status_update.py
+++ koalixcrm/crm/tests/test_task_update_last_status_update.py
@@ -1,5 +1,4 @@
 import datetime
-import pytest
 import pytz
 from django.test import TestCase
 from koalixcrm.crm.factories.factory_user import AdminUserFactory
@@ -14,47 +13,7 @@ from koalixcrm.crm.factories.factory_task import StandardTaskFactory
 from koalixcrm.crm.factories.factory_estimation import StandardHumanResourceEstimationToTaskFactory
 
 
-@pytest.fixture()
-def freeze(monkeypatch):
-    """ Now() manager patches date return a fixed, settable, value
-        (freezes date)
-    """
-    original = datetime.date
-
-    class FreezeMeta(type):
-        def __instancecheck__(self, instance):
-            if instance.isinstance(original) or instance.isinstance(Freeze):
-                return True
-
-    class Freeze(datetime.datetime):
-        __metaclass__ = FreezeMeta
-
-        @classmethod
-        def freeze(cls, val):
-            cls.frozen = val
-
-        @classmethod
-        def today(cls):
-            return cls.frozen
-
-        @classmethod
-        def delta(cls, timedelta=None, **kwargs):
-            """ Moves time fwd/bwd by the delta"""
-            from datetime import timedelta as td
-            if not timedelta:
-                timedelta = td(**kwargs)
-            cls.frozen += timedelta
-
-    monkeypatch.setattr(datetime, 'date', Freeze)
-    Freeze.freeze(original.today())
-    return Freeze
-
-
 class TaskUpdateLastStatusUpdate(TestCase):
-
-    @pytest.fixture(autouse=True)
-    def freeze_time(self, freeze):
-        self._freeze = freeze
 
     def setUp(self):
         datetime_now = datetime.datetime(2024, 1, 1, 0, 00)
@@ -87,8 +46,7 @@ class TaskUpdateLastStatusUpdate(TestCase):
     def test_task_last_status_update(self):
         previous_last_status_change = self.test_1st_task.last_status_change
         new_status = DoneTaskStatusFactory.create()
-        self._freeze.freeze(datetime.date(2024, 6, 2))
         self.test_1st_task.status = new_status
         self.test_1st_task.save()
         self.assertEquals(previous_last_status_change, datetime.date(2024, 6, 15))
-        self.assertEqual(self.test_1st_task.last_status_change, datetime.date(2024, 6, 2))
+        self.assertEqual(self.test_1st_task.last_status_change, datetime.date.today())
