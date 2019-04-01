d6d9dd9addd6c801230833f51c9fcb6279976cce
Aaron Riedener <aaron.riedener@gmail.com>
2019-04-01 23:33:03 +0200

    [Issue #274] added the effective effort to the task serialization to xml [Issue #274] updated the project report template

--- koalixcrm/crm/reporting/task.py
+++ koalixcrm/crm/reporting/task.py
@@ -353,13 +353,21 @@ class Task(models.Model):
             main_xml = PDFExport.merge_xml(main_xml, work_xml)
         main_xml = PDFExport.append_element_to_pattern(main_xml,
                                                        "object/[@model='crm.task']",
-                                                       "Effective_Effort_Overall",
+                                                       "Effective_Costs_Overall",
                                                        self.effective_costs(reporting_period=None))
+        main_xml = PDFExport.append_element_to_pattern(main_xml,
+                                                       "object/[@model='crm.task']",
+                                                       "Effective_Effort_Overall",
+                                                       self.effective_effort(reporting_period=None))
         if reporting_period:
             main_xml = PDFExport.append_element_to_pattern(main_xml,
                                                            "object/[@model='crm.task']",
-                                                           "Effective_Effort_InPeriod",
+                                                           "Effective_Costs_InPeriod",
                                                            self.effective_costs(reporting_period=reporting_period))
+            main_xml = PDFExport.append_element_to_pattern(main_xml,
+                                                           "object/[@model='crm.task']",
+                                                           "Effective_Effort_InPeriod",
+                                                           self.effective_effort(reporting_period=reporting_period))
         main_xml = PDFExport.append_element_to_pattern(main_xml,
                                                        "object/[@model='crm.task']",
                                                        "Planned_Effort",
