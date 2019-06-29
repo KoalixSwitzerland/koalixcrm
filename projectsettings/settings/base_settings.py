44290e3f25d22ad0475d6a579411e242be9e428c
Aaron Riedener <aaron.riedener@gmail.com>
2019-06-29 22:35:18 +0200

    [Issue #287] changed from relative path to an undefined origin to an absolute path

--- projectsettings/settings/base_settings.py
+++ projectsettings/settings/base_settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'projectsettings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./koalixcrm/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
